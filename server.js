c52fe463f514b106552957bbdc1db0410e214317
Isaiah <greatpup1940@gmail.com>
2020-08-27 23:40:08 -0400

    Updated server for heroku

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const app = require('./app')
 
+
 dotenv.config({ path: './config.env' })
 
+const port = process.env.PORT || 3000
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
 mongoose.connect(DB, {
@@ -16,6 +18,6 @@ mongoose.connect(DB, {
 })
 
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000')
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
 })

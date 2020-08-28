50018113e94e33b803876734db1a6da7e9d84585
Isaiah <greatpup1940@gmail.com>
2020-08-28 00:35:28 -0400

    Removed authorization to debug heroku

--- routes/ailmentRoutes.js
+++ routes/ailmentRoutes.js
@@ -3,7 +3,6 @@ const ailmentController = require('./../controllers/ailmentController');
 const authController = require('../controllers/authController')
 const router = express.Router();
 
-router.use(authController.protect)
 
 router.
     route('/')
